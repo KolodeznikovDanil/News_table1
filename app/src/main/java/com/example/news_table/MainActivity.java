a59d4e2af9bfeef8321d1d94538ed80eedc9fcd0
Danil <dkmori@mail.ru>
2022-03-01 15:37:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/news_table/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.news_table;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+    int id = item.getItemId();
+    
+}
